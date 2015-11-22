dda80758db0e56263c4cfec654a9319b2f1c1abb
Avi Halachmi (:avih) <avihpit@yahoo.com>
2015-11-22 06:57:10 +0200

    win: build batch: add some configurability, add tcclib.h
    - Allow CC and compile flags (WINTCCFLAGS) to be defined externally.
    - If TCCSTATIC is defined, creates a statically linked libtcc and tcc.
    - Added tcclib.h to include/ (needed for examples/libtcc_test.c etc).
    - Added some comments and info.

--- win32/build-tcc.bat
+++ win32/build-tcc.bat
@@ -3,6 +3,13 @@
 @rem batch file to build tcc using mingw gcc
 @rem ----------------------------------------------------
 
+@rem optional variables:
+@rem   TCCSTATIC - (default not defined) if defined e.g. to 1, will create static tcc and libtcc
+@rem   CC - (default 'gcc') - C compiler. setting this to a tcc executable works fine.
+@rem   WINTCCFLAGS - compiler flags, defaults to gcc flags, but tcc won't complain.
+
+pushd %~dp0
+
 @set /p VERSION= < ..\VERSION
 echo>..\config.h #define TCC_VERSION "%VERSION%"
 
@@ -10,15 +17,20 @@ echo>..\config.h #define TCC_VERSION "%VERSION%"
 @if _%1_==_x64_ shift /1 && goto x86_64
 
 @set target=-DTCC_TARGET_PE -DTCC_TARGET_I386
-@set CC=gcc -Os -s -fno-strict-aliasing -Wno-incompatible-pointer-types
-@if _%1_==_debug_ set CC=gcc -g -ggdb
+@if not defined WINTCCFLAGS (@set WINTCCFLAGS=-Os -s -fno-strict-aliasing -Wno-incompatible-pointer-types)
+@if not defined CC (@set CC=gcc)
+@if _%1_==_debug_ set WINTCCFLAGSCC=-g -ggdb
+@set CC=%CC% %WINTCCFLAGS%
 @set P=32
 @goto tools
 
 :x86_64
 @set target=-DTCC_TARGET_PE -DTCC_TARGET_X86_64
-@set CC=x86_64-w64-mingw32-gcc -Os -s -fno-strict-aliasing -Wno-incompatible-pointer-types
-@if _%1_==_debug_ set CC=x86_64-w64-mingw32-gcc -g -ggdb
+@rem mingw 64 has an ICE with -Os
+@if not defined WINTCCFLAGS (@set WINTCCFLAGS=-O0 -s -fno-strict-aliasing -Wno-incompatible-pointer-types)
+@if not defined CC (@set CC=x86_64-w64-mingw32-gcc)
+@if _%1_==_debug_ set WINTCCFLAGSCC=-g -ggdb
+@set CC=%CC% %WINTCCFLAGS%
 @set P=64
 @goto tools
 
@@ -29,9 +41,18 @@ echo will use %CC% %target%
 
 :libtcc
 if not exist libtcc mkdir libtcc
-copy ..\libtcc.h libtcc\libtcc.h > nul
-%CC% %target% -shared -DLIBTCC_AS_DLL -DONE_SOURCE ../libtcc.c -o libtcc.dll -Wl,-out-implib,libtcc/libtcc.a
-tiny_impdef libtcc.dll -o libtcc/libtcc.def
+copy ..\libtcc.h libtcc\libtcc.h
+copy ..\tcclib.h include\tcclib.h
+if not defined TCCSTATIC (
+  @rem Fine with CC as tcc, but with gcc from MSYS2, the resulting tcc needs libgcc_s_dw2-1.dll - usable in MSYS2
+  %CC% %target% -shared -DLIBTCC_AS_DLL -DONE_SOURCE ../libtcc.c -o libtcc.dll -Wl,-out-implib,libtcc/libtcc.a
+  tiny_impdef libtcc.dll -o libtcc/libtcc.def
+  @if exist libtcc.def del libtcc.def
+) else (
+  @rem Currently this works only with CC as tcc
+  %CC% %target% -c -DONE_SOURCE ../libtcc.c -o libtcc/libtcc.a
+  @rem ar rcs libtcc/libtcc.a libtcc/libtcc.o
+)
 
 :tcc
 %CC% %target% ../tcc.c -o tcc.exe -ltcc -Llibtcc
